b8147cffb866f1ec8bb4fcc0bd3573b3dcb574d7
TodorovIT <120493772+TodorovIT@users.noreply.github.com>
2023-05-23 14:40:13 +0300

    Add files via upload

---
+++ AI/Program.cs
@@ -0,0 +1,63 @@
+﻿
+using Newtonsoft.Json;
+using System;
+using System.Globalization;
+using System.Net.Http.Headers;
+
+class Program
+{
+
+    static void Main()
+    {
+        while (true)
+        {
+            Console.WriteLine();
+            Console.WriteLine("------------------------------------------------------------------------------------------------------------------------");
+            Console.WriteLine();
+            Console.WriteLine("Ask a Question");
+            Console.WriteLine();
+            var question = Console.ReadLine();
+
+            var answer = callOpenAI(2000, question, "text-davinci-003", 0.7, 1, 0, 0);
+            Console.WriteLine(answer);
+        }
+
+
+    }
+
+    private static string callOpenAI(int tokens, string input, string engine,
+          double temperature, int topP, int frequencyPenalty, int presencePenalty)
+    {
+        var openAiKey = "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";
+        var apiCall = "https://api.openai.com/v1/engines/" + engine + "/completions";
+        try
+        {
+            using (var httpClient = new HttpClient())
+            {
+                using (var request = new HttpRequestMessage(new HttpMethod("POST"), apiCall))
+                {
+                    request.Headers.TryAddWithoutValidation("Authorization", "Bearer " + openAiKey);
+                    request.Content = new StringContent("{\n  \"prompt\": \"" + input + "\",\n  \"temperature\": " +
+                                                        temperature.ToString(CultureInfo.InvariantCulture) + ",\n  \"max_tokens\": " + tokens + ",\n  \"top_p\": " + topP +
+                                                        ",\n  \"frequency_penalty\": " + frequencyPenalty + ",\n  \"presence_penalty\": " + presencePenalty + "\n}");
+                    request.Content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/json");
+                    var response = httpClient.SendAsync(request).Result;
+                    var json = response.Content.ReadAsStringAsync().Result;
+
+                    dynamic dynObj = JsonConvert.DeserializeObject(json);
+
+                    if (dynObj != null)
+                    {
+                        return dynObj.choices[0].text.ToString();
+                    }
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        return null;
+    }
+    //
+}
